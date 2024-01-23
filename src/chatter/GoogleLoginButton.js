1687da2aaf36a6e8b9bf1e07f06f6f582677b726
Ramsy4754 <ramsy4754@linarena.com>
2024-01-23 17:27:49 +0900

    google auth(2)

--- src/chatter/GoogleLoginButton.js
+++ src/chatter/GoogleLoginButton.js
@@ -3,8 +3,26 @@ import {useGoogleLogin} from "@react-oauth/google";
 const GoogleLoginButton = () => {
 
     const googleLogin = useGoogleLogin({
-        onSuccess: (codeResponse) => {
-            console.log(codeResponse)
+        onSuccess: async (codeResponse) => {
+            const data = {
+                code: codeResponse.code,
+            };
+            console.log(data);
+
+            try {
+                const response = await fetch(process.env.REACT_APP_USER_SERVER_URL, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                });
+
+                const responseData = await response.json();
+                console.log('Server response:', responseData);
+            } catch (error) {
+                console.error('Error sending data to server:', error);
+            }
         },
         onError: errorResponse => console.log(errorResponse),
         flow: 'auth-code',
